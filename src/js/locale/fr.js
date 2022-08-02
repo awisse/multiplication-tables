23726cbd017e93d6a9bbaea5d0b14dd410de485c
Aurel Wisse <4907846+awisse@users.noreply.github.com>
2022-08-01 20:47:11 -0400

    Better translation of French text.

--- src/js/locale/fr.js
+++ src/js/locale/fr.js
@@ -1,10 +1,10 @@
 export default {
 	pageTitle              : "Tables de multipliction",
 	metaDescription        : "Apprends les multiplication avec ce jeu",
-	endMessage             : "Excellent. Tu a complété toutes les tables avec le résultat %d",
+	endMessage             : "Tu termines avec le résultat %d",
 	whatsYourName          : "Quel est ton nom?",
-	whichMultiTable        : "Quel table voulez vous pratiquer?",
-	letsStart              : "Allons-y!",
+	whichMultiTable        : "Quel(les) table(s) voulez vous pratiquer?",
+	letsStart              : "Commencer",
 	noticeSelectAtLeastOne : "Choisir au moins une table",
 	noticeInsertYourName   : "Allô! Votre nom svp :)",
 	howMuchIs              : "Combien fait "
