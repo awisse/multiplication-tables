d45aea9216456606fc58c56903cc9bc381c55b3f
Aurel Wisse <4907846+awisse@users.noreply.github.com>
2022-08-11 09:56:29 -0400

    Start page. Missing files in previous commit.
    1. New core script `script.js`.
    
    5. Start programming start page: Layout (view.js) Style (style.css) and data
       (model.js).

--- model.js
+++ model.js
@@ -5,6 +5,95 @@
  * 4. Load from localStorage
  * 5. Save to localStorage
  * */
+import {TESTING, MAX_TABLE_INT} from './constants.js'
+
+class Model {
+
+    constructor() {
+    }
+
+
+    getPlayersAndScores() {
+        /* Retrieve names and high scores from localStorage */
+        var players;
+        if (TESTING) {
+            players = testPlayers()
+        } else {
+            players = getStorageItem(PLAYERS)
+        }
+
+        players.sort(playerSort)
+
+        return players
+    }
+
+}
+
+function testPlayers() {
+    /* Une liste de noms et scores pour tester */
+    // return [];
+    let players = [ {name: 'Maman', 
+                     score: 10754,
+                     combinations: generateMultitables()},
+                    {name: 'Florence',
+                     score: 756,
+                     combinations: generateMultitables()}, 
+                    {name: 'Rémi',
+                     score: 756,
+                     combinations: generateMultitables()}];
+    return players;
+}
+
+function playerSort(a, b) {
+    /* Sort players by score. Highest first.
+     * Then by name. */
+    if (a.score > b.score) { return -1; }
+    if (a.score < b.score) { return 1; }
+    if (a.score === b.score) {
+        if (a.name < b.name) { return -1; }
+        if (a.name > b.name) { return 1; }
+        return 0;
+    }
+}
+
+function generateMultitables(repetitions=3) {
+    /* Generate a list with all combinations of multiplications starting
+     * with multiples of 2. Each combination is repeated `repetition` times
+     * in order to determine its probability of being selected. 
+     * If an answer is correct, one instance of the combination is removed.
+     * If an answer is wrong, one instance of the combination is added. */
+
+	const allCombinations = [];
+	for (let i = 2; i < MAX_TABLE_INT; i++) {
+		for (let j = 2; j <= MAX_TABLE_INT; j++) {
+            for (let k=0; k<3; k++) {
+                allCombinations.push([i, j]);
+                // Ask question in both directions
+                if (i != j) {
+                    allCombinations.push([j, i])
+                }
+            }
+		}
+	}
+	return allCombinations;
+}
+
+
+function getStorageItem(name) {
+    /* Get the item named `name` from localStorage */
+
+    if (!localStorage)
+        return null;
+    let value = localStorage.getItem(name);
+    return value ? JSON.parse(value) : null;
+}
+
+function setStorageItem(name) {
+    
+    if (localStorage) {
+        localStorage.setItem(name, JSON.stringify(value));
+    }
+}
 function computePairs(selectedTables) {
     /* Compute all pairs of numbers for the selected tables */
 }
@@ -26,4 +115,12 @@ function selectRandomPair() {
 function checkAnswer(pair, user_selection) {
     /* Check whether the user provided the correct answer */
     return (pair[0] * pair[1] === user_selection)
+}
+
+export { 
+    Model, 
+    /* Export for testing only */
+    testPlayers,
+    playerSort,
+
 }
--- view.js
+++ view.js
@@ -2,6 +2,127 @@
  * 1. Displaying data and controls to the user
  * 2. Playing sounds 
 */
+import locale from './locale/default.js'
+
+class View {
+
+    constructor(title) {
+
+        this.title = this.getElement('#mainTitle');
+        this.main = this.getElement('#gameboard');
+        this.footer = this.getElement('#footer');
+
+        this.hideFooter();
+
+        this.setTitle(locale.pageTitle);
+
+        /* Create start page objects ------------------------------*/
+        // Title of start page
+        this.namePageTitle = createElement('h1')
+        this.namePageTitle.textContent = locale.usersAndScores;
+
+        // Create input box for new name
+        this.nameInput = createElement('input');
+        this.nameInput.placeholder = locale.whatsYourName;
+        this.nameInput.name = 'username';
+
+        // Create list for existing names
+        this.nameList = createElement('ul', 'name-list');
+
+        /* Create table selection page objects --------------------*/
+
+
+        /* Create quiz page objects -------------------------------*/
+        
+
+
+
+    }
+
+    getElement(selector) {
+        const element = document.querySelector(selector);
+
+        return element;
+    }
+
+    setTitle(title) {
+        this.title.textContent = title;
+    }
+
+    hideFooter() {
+        this.footer.classList.add('hidden')
+    }
+    
+    showFooter() {
+        this.footer.classList.remove('hidden')
+    }
+    
+    emptyMainSection() {
+        /* Empty the main section of all elements. */
+        while (this.main.firstChild) {
+            this.main.removeChild(this.main.firstChild);
+        }
+    }
+
+    showStartPage(names) {
+        /* Show the start page.
+         * Create a listbox with `names` to choose from or enter a new name.
+         * Show highscores. */
+
+        // Empty the page first
+        this.emptyMainSection();
+        
+        // Create the list of names to display
+        // Empty the list first
+        while (this.nameList.firstChild) { 
+            this.nameList.removeChild(this.nameList.firstChild)
+        }
+
+        if (names.length === 0) {
+            const p = createElement('p');
+            p.textContent = 'Aucun joueur enregistré! Ajoutez un joueur?';
+            this.nameList.append(p);
+        }
+        else {
+            names.forEach(user => {
+                const li = createElement('li', 'names');
+
+                const select = createElement('input', 'name-select');
+                select.type = 'radio';
+                select.name = 'name-select';
+                select.value = user.name;
+                select.id = user.name + "_id";
+
+                const label = createElement('label', 'username');
+                label.textContent = user.name;
+                label.htmlFor = user.name + "_id";
+
+                const score = createElement('span', 'userscore');
+                score.textContent = user.score;
+
+                li.append(select, label, score);
+                this.nameList.append(li);
+            });
+        }
+
+        this.nameList.firstChild.getElementsByTagName("input")[0].checked = true;
+
+        // Put the page together
+        this.main.append(this.namePageTitle, this.nameInput, this.nameList)
+    }
+
+
+}
+
+
+function createElement(tag, className) {
+const element = document.createElement(tag)
+
+if (className) element.classList.add(className)
+
+return element
+}
+
 function displayStartPage(container) {
     /* 1. Display the start page with previous high scores.
      * 2. Display "Add User" button 
@@ -51,3 +172,5 @@ function loadResultPage(container, result) {
 
 
 }
+
+export { View }
